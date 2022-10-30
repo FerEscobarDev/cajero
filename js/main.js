928462de496358b91a723463cf42767007bd4c1f
keishmen <57463087+keishmen@users.noreply.github.com>
2022-10-29 23:31:43 -0500

    finalizado

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
 const saludo = document.querySelector('#name');
 const modal = document.querySelector('#modal')
 const titleModal = document.querySelector('#titleModal');
-const bodyModal = document.querySelector('#bodyModal')
-const user = JSON.parse(sessionStorage.getItem('user'));
-const { nombre, log } = user;
+const bodyModal = document.querySelector('#bodyModal');
+
+//Construcción de formulario
+const form = document.createElement('form');
+const div = document.createElement('div');
+const error = document.createElement('div');
+const label = document.createElement('label');
+const input = document.createElement('input');
+const submit = document.createElement('button');
+div.className = "inputForm";
+error.className = "errorForm hidden";
+label.textContent = "Monto";
+input.className = "inputModal";
+input.type = "number";
+input.name = "mount";
+submit.className = "btn";
+form.className = "formModal";
+div.appendChild(label);
+div.appendChild(input);
+form.appendChild(div);
+form.appendChild(submit);
 
 
-document.addEventListener('DOMContentLoaded', function(){    
+document.addEventListener('DOMContentLoaded', function(){ 
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    const { nombre } = user;   
     saludo.innerText = nombre;
 })
 
 //Ver movimientos
 document.querySelector('#transactions').addEventListener('click', () => {
     openModal();
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    const { log } = user;  
     titleModal.textContent = 'Movimientos'
     const buildingBody = document.createDocumentFragment();
     const titleTab = document.createElement('div');
@@ -27,26 +49,113 @@ document.querySelector('#transactions').addEventListener('click', () => {
         div.innerHTML = `<span>${dateFormat}</span><span>${movimiento.monto}</span><span>${movimiento.type}</span>`
         buildingBody.appendChild(div);    
     }
+    bodyModal.appendChild(buildingBody);    
+});
+
+//Modal retiros
+document.querySelector('#withdrawals').addEventListener('click', () => {
+    openModal();
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    const { saldo, log } = user;  
+    titleModal.textContent = 'Retirar Saldo';
+    const buildingBody = document.createDocumentFragment();
+    
+    submit.textContent = 'Retirar';
+    submit.id = "withdrawalSubmit";
+    form.id = "formWithdrawal";
+    input.value = "";
+
+    buildingBody.appendChild(form);
     bodyModal.appendChild(buildingBody);
-    
-    console.log(bodyModal.children.length)
-    
-    if(bodyModal.children.length > 0)
-    {
-        /* for(let i = 0; bodyModal.children.length > 0; i++)
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const data = new FormData(form);
+
+        const values = Object.fromEntries(data);
+
+        const newBalance = saldo - values.mount;
+
+        if(newBalance < 10)
         {
-            console.log(bodyModal.children[i])
-            //child.remove();
-        } */
-    }
-})
+            form.appendChild(error);
+            error.textContent = 'El monto de tu retiro supera el saldo mínimo de tu cuenta.';
+            error.classList.remove('hidden');
+        }
+        else
+        {
+            log.unshift({fecha: new Date().toISOString(), monto: Number(values.mount), type: 'retiro'});
+            const userUpdate = {
+                ...user,
+                saldo: newBalance,
+                log: log,
 
+            }
+            window.sessionStorage.setItem('user', JSON.stringify(userUpdate)); 
+            closeModal();
+        }
+    });
+});
+
+//Modal depósitos
+document.querySelector('#deposits').addEventListener('click', () => {
+    openModal();
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    const { saldo, log } = user; 
+    titleModal.textContent = 'Depositar Saldo';
+    const buildingBody = document.createDocumentFragment();
     
+    submit.textContent = 'Depósitar';
+    submit.id = "depositSubmit";
+    form.id = "formDeposit";
+    input.value = "";
 
-/*  */
+    buildingBody.appendChild(form);
+    bodyModal.appendChild(buildingBody);
 
+    form.addEventListener('submit', e => {
+        e.preventDefault();
 
+        const data = new FormData(form);
 
+        const values = Object.fromEntries(data);
+
+        const newBalance = Number(saldo) + Number(values.mount);
+
+        if(newBalance > 990)
+        {
+            form.appendChild(error);
+            error.textContent = 'El monto de tu depósito supera el saldo máximo de tu cuenta.';
+            error.classList.remove('hidden');
+        }
+        else
+        {
+            log.unshift({fecha: new Date().toISOString(), monto: Number(values.mount), type: 'depósito'});
+            const userUpdate = {
+                ...user,
+                saldo: newBalance,
+                log: log,
+
+            }
+            window.sessionStorage.setItem('user', JSON.stringify(userUpdate)); 
+            closeModal();
+        }
+    });
+});
+
+//Modal saldo
+document.querySelector('#balance').addEventListener('click', () => {
+    openModal();
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    const { saldo } = user;  
+    titleModal.textContent = 'Tu Saldo';
+    const h1Saldo = document.createElement('h1');
+    h1Saldo.textContent = `$ ${saldo}`;
+
+    bodyModal.appendChild(h1Saldo);
+
+})
 
 
 
@@ -67,11 +176,19 @@ const openModal = () => {
 //Cierra Modal
 const closeModal = () => {
     modal.classList.add('hidden');
+    if(bodyModal.children.length > 0)
+    {
+        for(let i = bodyModal.children.length - 1; i >= 0; i--)
+        {
+            bodyModal.children[i].remove();
+        }
+    }
+    error.classList.add('hidden');
 }
 
 //Cierra Modal al hacer click fuera del contenedor modal
 modal.addEventListener('click', () => {
-    modal.classList.toggle('hidden');
+    closeModal();
 })
 
 //Evita el cierre al hacer click dentro del modal
@@ -81,6 +198,6 @@ const contentModal = document.querySelector('#contentModal').addEventListener('c
 
 //Cierra Modal desde botón
 const buttonClose = document.querySelector('#close').addEventListener('click', () => {
-    modal.classList.toggle('hidden');
+    closeModal();
 })
 
